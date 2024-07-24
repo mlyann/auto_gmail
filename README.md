8b8552f5d2a85870e26a5cacab99cd9587c93395
Minglai Yang <150297217+mlyann@users.noreply.github.com>
2024-07-24 10:09:49 -0400

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# auto_gmail
-A project sending AUTO update Gmail everyday.
+# In-office Email Script
+
+
+## Use your own API Key:
+API_KEY = "SG.I2........"
+.env file to keep key secure
+
+
+##Install required libraries
+pip install "libraries"
+
+##Run with crontab
+Cron config:
+0 18 * * 0-4 PATH=/usr/local/bin:/usr/bin:/bin /usr/bin/python3 absolute/path/in-office-email-script >> /absolute/path/for/logfile.log 2>&1
+
+#Email will send at 6pm sun-thurs
+
+pro? Just edit this README.md and make it your own. Want to make it easy? [Use the template at the bottom](#editing-this-readme)!
+
+## Authors and acknowledgment
+Show your appreciation to those who have contributed to the project.
+
+## License
+For open source projects, say how it is licensed.
+
+## Project status
+If you have run out of energy or time for your project, put a note at the top of the README saying that development has slowed down or stopped completely. Someone may choose to fork your project or volunteer to step in as a maintainer or owner, allowing your project to keep going. You can also make an explicit request for maintainers.
---
+++ scripts/coretechs-office-planning-email.py
@@ -0,0 +1,106 @@
+import json
+import os
+from datetime import datetime, timedelta
+
+import requests
+from dotenv import load_dotenv
+from requests.auth import HTTPBasicAuth
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail, Email, To, Bcc, HtmlContent
+
+
+load_dotenv()
+api_key = os.getenv('API_KEY')
+if not api_key:
+    raise ValueError("API_KEY is not set in the environment variables.")
+
+sg = SendGridAPIClient(api_key=api_key)
+# from_email = Email("coretechs.twilio@coretechs.com")
+from_email = Email("FROM_EMAIL_ADDRESS")
+
+
+def fetch_data(url):
+    response = requests.get(url, auth=HTTPBasicAuth("why?", "whynot!"))
+    if response.status_code == 200:
+        return response.json()
+
+    raise Exception(f"Failed to fetch data: {response.status_code} - {response.content}")
+
+def send_email(data, date_str):
+    to_emails = [To("RECEIVE_EMAIL_ADDRESS")]
+    bcc_emails = Bcc("BCC_EMAIL_ADDRESS")
+    subject = f"???????????????????????? - {date_str} - Count: {len(data)}"
+    html_content_str = f"""
+    <html>
+    <head>
+        <style>
+            .card {{
+                display: inline-block;
+                margin: 10px;
+                width: 200px;
+                border: 1px solid #ccc;
+                border-radius: 5px;
+                padding: 10px;
+                text-align: center;
+            }}
+            .card img {{
+                width: 100px;
+                border-radius: 50%;
+            }}
+            .image-wrapper.circle {{
+                width: 150px;
+                height: 150px;
+                margin: 0 auto; /* Centers the circle horizontally */
+                display: flex;
+                align-items: center; /* Centers content vertically */
+                justify-content: center; /* Centers content horizontally */
+                -webkit-border-radius: 50%;
+                -moz-border-radius: 50%;
+                border-radius: 50%;
+                background-size: cover; /* Cover the entire area of the circle */
+                background-position: center; /* Center the background image */
+                overflow: hidden; /* Hide anything outside the circle */
+                border: none;
+            }}
+            .image-wrapper.circle img {{   
+                max-width: 100%;
+                max-height: 100%;
+                object-fit: cover; /* This will cover the area without losing the aspect ratio */
+            }}
+
+
+        </style>
+    </head>
+    <body>
+        <a href="THE_LINK" style="background-color: #4CAF50; color: white; padding: 10px 20px; text-align: center; text-decoration: none; display: inline-block; font-size: 16px; margin: 4px 2px; cursor: pointer; border-radius: 12px;">LINK_DESCRIPTION</a>
+        <h1>{subject}</h1>
+        <div class="card-container">"""
+
+    for person in data:
+        image_url = "EXCEPTION_IMAGE" if person['name'] == "EXCEPTION_USER_NAME" else person['image']
+        html_content_str += f""" <div class="card">
+        <div class="image-wrapper circle" style="background-image: url({image_url});"></div>
+        <p>{person['name']}</p>
+    </div> """
+    html_content_str += """
+        </div>
+        <a href="THE_LINK" style="background-color: #4CAF50; color: white; padding: 10px 20px; text-align: center; text-decoration: none; display: inline-block; font-size: 16px; margin: 4px 2px; cursor: pointer; border-radius: 12px;">LINK_DESCRIPTION</a>
+    </body>
+    </html>
+    """
+
+    html_content = HtmlContent(html_content_str)
+    mail = Mail(from_email, to_emails, subject, html_content)
+    mail.add_bcc(bcc_emails)
+    response = sg.send(mail)
+    print(response.status_code, response.body, response.headers)
+
+def main():
+    json_url = "CREAT_YOUR_OWN_JSON.json"
+    data = fetch_data(json_url)
+    current_date = datetime.now() + timedelta(days=1)
+    formatted_date = current_date.strftime("%a %b %d")  
+    send_email(data[formatted_date], formatted_date)
+
+        
+main()
